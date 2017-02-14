ab09f22308764bb91f5b2b06796efbf59e19101f
ppoteralski <piotr.poteralski@rst-it.com>
2017-02-14 22:04:53 +0100

    test

---
+++ tests/base.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from abc import abstractmethod
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+class BaseTest(TestCase):
+    def setUp(self):
+        self.engine = create_engine('postgresql+psycopg2://localhost:5432/test')
+        self.Base = declarative_base()
+        Session = sessionmaker(bind=self.engine)
+        self.define_models()
+
+        self.session = Session()
+        self.Base.metadata.create_all(self.engine)
+
+    @abstractmethod
+    def define_models(self): pass
+
+    def tearDown(self):
+        self.Base.metadata.drop_all(self.engine)
---
+++ tests/test_fields.py
@@ -0,0 +1,26 @@
+from .base import BaseTest
+from sqlalchemy_stdimage.fields import StdImageField
+from sqlalchemy import Column, Integer
+
+TEMP_IMAGES_DIR = 'temp_images/'
+
+class TestTimestamp(BaseTest):
+
+    def define_models(self):
+        class User(self.Base):
+            __tablename__ = 'user'
+            id = Column(Integer, primary_key=True)
+            avatar = Column(StdImageField(), nullable=False)
+        self.User = User
+
+    def test_create_instance(self):
+        u = self.User()
+        self.session.add(u)
+        self.session.commit()
+
+    def test_upload(self):
+        with open(TEMP_IMAGES_DIR + 'python_logo.png', 'rb') as file:
+            u = self.User()
+            u.avatar = file
+            self.session.add(u)
+            self.session.commit()
