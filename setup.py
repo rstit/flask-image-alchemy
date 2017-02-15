c6efafc3cd9776a2aa7b32fc2cca1bd5355d7720
ppoteralski <piotr.poteralski@rst-it.com>
2017-02-15 22:23:36 +0100

    setup py

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+Flask-ImageAlchemy
+-------------
+SQLAlchemy Standarized Image Field for Flask
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-ImageAlchemy',
+    version='0.0.1',
+    url='https://github.com/rstit/flask-image-alchemy',
+    license='BSD',
+    author='RST-IT',
+    author_email='piotr.poteralski@rst-it.com',
+    description='SQLAlchemy Standarized Image Field for Flask',
+    long_description=__doc__,
+    packages=['flask_image_alchemy'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['SQLAlchemy', 'wand', 'boto3'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
