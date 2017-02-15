f7bea73f11e03388a8d2ca290758fcff09a3decd
ppoteralski <piotr.poteralski@rst-it.com>
2017-02-15 13:09:07 +0100

    fix

--- sqlalchemy_stdimage/storages.py
+++ sqlalchemy_stdimage/storages.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
+from os import makedirs
 
-from wand.image import Image
+from os.path import exists, split
 
 
 class BaseStorage:
@@ -26,10 +27,18 @@ class S3Storage(BaseStorage):
 
 
 class FileStorage(BaseStorage):
+
+    def _create_dir_if_needed(self, file_name):
+        directory, _ =  split(file_name)
+        print(directory)
+        if directory and not exists(directory):
+            makedirs(directory)
+
     def read(self, file_name):
         with open(file_name, 'r') as file:
             return file.read()
 
     def write(self, data, file_name):
+        self._create_dir_if_needed(file_name)
         with open(file_name, 'wb+') as file:
             file.write(data)
