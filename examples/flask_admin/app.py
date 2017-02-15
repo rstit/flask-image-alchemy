c91da844c5f38467ff0696a27df737990310b11c
ppoteralski <piotr.poteralski@rst-it.com>
2017-02-15 17:28:25 +0100

    index

--- examples/flask_admin/app.py
+++ examples/flask_admin/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_admin import Admin
+from werkzeug.utils import secure_filename
+from flask_wtf import Form
+from flask_wtf.file import FileField
+
 from flask_image_alchemy.storages import S3Storage
 from flask_image_alchemy.fields import StdImageField
 
@@ -9,7 +14,7 @@ admin = Admin()
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates')
 
     # config
     app.config['SECRET_KEY'] = '123456790'
@@ -26,12 +31,6 @@ def create_app():
 app = create_app()
 
 
-# define main route
-@app.route('/')
-def index():
-    return 'Hello World'
-
-
 # define models
 class ExampleModel(db.Model):
     image = db.Column(
@@ -42,6 +41,22 @@ class ExampleModel(db.Model):
             }
         )
     )
+
+
+# define forms
+class ExampleModelForm(Form):
+    image = FileField('Your photo')
+
+# define routes
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    form = ExampleModelForm()
+    if form.validate_on_submit():
+        filename = secure_filename(form.image.data.filename)
+    else:
+        filename = None
+    return render_template('upload.html', form=form, filename=filename)
+
 
 def build_db():
     db.drop_all()
