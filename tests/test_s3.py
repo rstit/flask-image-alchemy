a5a2c128a6a29559db1f0b6aa549e69bf684f73f
ppoteralski <piotr.poteralski@rst-it.com>
2017-02-15 17:55:29 +0100

    submit

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -8,55 +8,55 @@ from sqlalchemy import Column, Integer
 
 TEMP_IMAGES_DIR = 'temp_images/'
 AMAZON_SERVICE_NAME = 's3'
-AWS_ACCESS_KEY = 'f21ec2b9feb6baf9b5edcc473d8838d9e6a69ade'
-AWS_SECRET = '378b4bd1a5984f891065cdadf3a31d8c6cb4f5eda8863a3c'
+AWS_ACCESS_KEY = 'xxx'
+AWS_SECRET = 'xxx'
 AWS_REGION_NAME = 'eu-central-1'
 BUCKET_NAME = 'haraka-local'
-#
-#
-# class TestFieldVariations(BaseTest):
-#
-#     def setUp(self):
-#         self.client = client(
-#             AMAZON_SERVICE_NAME,
-#             aws_access_key_id=AWS_ACCESS_KEY,
-#             aws_secret_access_key=AWS_SECRET,
-#             region_name=AWS_REGION_NAME,
-#             config=Config(signature_version='s3v4')
-#         )
-#         super().setUp()
-#
-#     def define_models(self):
-#         class User(self.Base):
-#             __tablename__ = 'user'
-#             id = Column(Integer, primary_key=True)
-#             avatar = Column(
-#                 StdImageField(
-#                     storage=S3Storage(self.client),
-#                     variations={"thumbnail": {'height': 100, 'width': 100}}
-#                 ),
-#                 nullable=False
-#             )
-#         self.User = User
-#
-#     def test_create_instance(self):
-#         u = self.User()
-#         self.session.add(u)
-#         self.session.commit()
-#
-#     def test_upload(self):
-#         with open(TEMP_IMAGES_DIR + 'python_logo.png', 'rb') as file:
-#             u = self.User()
-#             u.avatar = file
-#             self.session.add(u)
-#             self.session.commit()
-#             self.assertIsInstance(u.avatar, StdImageFile)
-#             self.assertIsNotNone(u.avatar.url)
-#             self.assertIsInstance(u.avatar.thumbnail, StdImageFile)
-#             self.assertIsNotNone(u.avatar.thumbnail.url)
-#
-#     def tearDown(self):
-#         for user in self.session.query(self.User):
-#             if user.avatar:
-#                 user.avatar.delete(all=True)
-#         super().tearDown()
+
+
+class TestFieldVariations(BaseTest):
+
+    def setUp(self):
+        self.client = client(
+            AMAZON_SERVICE_NAME,
+            aws_access_key_id=AWS_ACCESS_KEY,
+            aws_secret_access_key=AWS_SECRET,
+            region_name=AWS_REGION_NAME,
+            config=Config(signature_version='s3v4')
+        )
+        super().setUp()
+
+    def define_models(self):
+        class User(self.Base):
+            __tablename__ = 'user'
+            id = Column(Integer, primary_key=True)
+            avatar = Column(
+                StdImageField(
+                    storage=S3Storage(self.client),
+                    variations={"thumbnail": {'height': 100, 'width': 100}}
+                ),
+                nullable=False
+            )
+        self.User = User
+
+    def test_create_instance(self):
+        u = self.User()
+        self.session.add(u)
+        self.session.commit()
+
+    def test_upload(self):
+        with open(TEMP_IMAGES_DIR + 'python_logo.png', 'rb') as file:
+            u = self.User()
+            u.avatar = file
+            self.session.add(u)
+            self.session.commit()
+            self.assertIsInstance(u.avatar, StdImageFile)
+            self.assertIsNotNone(u.avatar.url)
+            self.assertIsInstance(u.avatar.thumbnail, StdImageFile)
+            self.assertIsNotNone(u.avatar.thumbnail.url)
+
+    def tearDown(self):
+        for user in self.session.query(self.User):
+            if user.avatar:
+                user.avatar.delete(all=True)
+        super().tearDown()
