795472a49e8929c42af1403dfafa9fbb33e6ff91
ppoteralski <piotr.poteralski@rst-it.com>
2017-02-20 13:03:19 +0100

    s3 client in context stack

--- flask_image_alchemy/storages/s3.py
+++ flask_image_alchemy/storages/s3.py
@@ -2,6 +2,13 @@ from boto3 import client
 from botocore.config import Config
 
 from flask_image_alchemy.storages.base import BaseStorage
+# Find the stack on which we want to store s3 client.
+# Starting with Flask 0.9, the _app_ctx_stack is the correct one,
+# before that we need to use the _request_ctx_stack.
+try:
+    from flask import _app_ctx_stack as stack
+except ImportError:
+    from flask import _request_ctx_stack as stack
 
 
 class S3Storage(BaseStorage):
@@ -10,6 +17,20 @@ class S3Storage(BaseStorage):
         if app:
             self.init_app(app, config=config)
 
+    @property
+    def client(self):
+        ctx = stack.top
+        if ctx is not None:
+            if not hasattr(ctx, 's3_service'):
+                ctx.s3_service = client(
+                    's3',
+                    aws_access_key_id=self.ACCESS_KEY,
+                    aws_secret_access_key=self.SECRET,
+                    region_name=self.REGION_NAME,
+                    config=self.config
+                )
+            return ctx.s3_service
+
     def init_app(self, app, config=None):
         self.app = app
         self.ACCESS_KEY = app.config.get('AWS_ACCESS_KEY_ID')
@@ -17,14 +38,6 @@ class S3Storage(BaseStorage):
         self.REGION_NAME = app.config.get('AWS_REGION_NAME')
         self.BUCKET_NAME = app.config.get('S3_BUCKET_NAME')
         self.config = config if config else Config(signature_version='s3v4')
-        self.client = client(
-            's3',
-            aws_access_key_id=self.ACCESS_KEY,
-            aws_secret_access_key=self.SECRET,
-            region_name=self.REGION_NAME,
-            config=self.config
-        )
-
 
     def read(self, file_name):
         self.client.download_fileobj(self.BUCKET_NAME, file_name)
