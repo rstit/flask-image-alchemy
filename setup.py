000e59bb2e5b483aaa5487a400c0a5ef7e4fc8de
ppoteralski <piotr.poteralski@rst-it.com>
2017-02-20 10:39:42 +0100

    version 0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='Flask-ImageAlchemy',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/rstit/flask-image-alchemy',
     license='BSD',
     author='RST-IT',
